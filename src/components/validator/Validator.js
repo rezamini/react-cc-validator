12eb9bf1dd4a84c8ae0e3d383398e977b7c9beab
rezamini <reza.amini.b@gmail.com>
2020-03-02 18:26:30 +0100

    add validator component

---
+++ src/components/validator/Validator.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function Validator() {
+
+
+  useEffect(() => {
+
+
+  }, []);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Validator;
