dbfa7b13d12fe6e284f2ddef7f4a43b03ee78956
rezamini <reza.amini.b@gmail.com>
2020-03-02 18:35:35 +0100

    add assets folder and logo files

--- src/components/validator/Validator.js
+++ src/components/validator/Validator.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Visa from '../../assets/visa.svg';
+import Mastercard from '../../assets/mastercard.svg';
+import Discover from '../../assets/discover.svg';
+import Amex from '../../assets/amex.svg';
 
 function Validator() {
 
@@ -11,7 +15,15 @@ function Validator() {
 
   return (
     <div>
-
+      <div className="input-addon">
+        <input type="text"/>
+        <button className="reset">reset</button>
+      </div>
+      <div className="error">
+        <span></span>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
